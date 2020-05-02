376ee68e46d53d4b3f1e9dd00d2a8503f5809e8b
DraftMan <nicovanaarsen@gmail.com>
2020-05-03 01:37:38 +0200

    default layout created

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,60 +15,69 @@ export default {
     components: {
         Header,
         Footer
+    },
+    head () {
+        return {
+            meta: [
+                { hid: 'robots', name: 'robots', content: 'noindex' }
+            ]
+        }
     }
 }
 </script>
 
-<style>
+<style lang="scss">
 
 * {
-  box-sizing: border-box;
-  font-family: "Open Sans", "Arial Narrow", Arial, sans-serif;
+    box-sizing: border-box;
+    font-family: "Open Sans", "Arial Narrow", Arial, sans-serif;
 }
 
 @font-face {
-  font-family: "Uni Sans";
-  src: local("Uni Sans Heavy"), local("UniSansHeavy"),
-    url("../assets/fonts/UniSans.woff2") format("woff2");
-  font-weight: 900;
-  font-style: normal;
+    font-family: 'Uni Sans';
+    src: url('../assets/fonts/UniSans.woff2') format('woff2'),
+        url('../assets/fonts/UniSans.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
 }
 
 .container{
     min-height: calc(100vh - 365px);
+    @media screen and (max-width: $phone){
+        padding-top: 70px;
+    }
 }
 
 a.link {
-  position: relative;
-  text-decoration: none;
-  color: #cd6e57;
-  -webkit-transition: color .15s ease-in;
-  -moz-transition: color .15s ease-in;
-  -o-transition: color .15s ease-in;
-  transition: color .15s ease-in;
+    position: relative;
+    text-decoration: none;
+    color: #cd6e57;
+    -webkit-transition: color .15s ease-in;
+    -moz-transition: color .15s ease-in;
+    -o-transition: color .15s ease-in;
+    transition: color .15s ease-in;
+    &:after {
+        content: '';
+        position: absolute;
+        bottom: -2px;
+        left: 50%;
+        width: 100%;
+        height: 1px;
+        background-color: #cd6e57;
+        -webkit-transition: -webkit-transform 250ms;
+        transition: -webkit-transform 250ms;
+        transition: transform 250ms;
+        transition: transform 250ms, -webkit-transform 250ms;
+        -webkit-transform: translateX(-50%) scaleX(0);
+        transform: translateX(-50%) scaleX(0);
+        -webkit-transform-origin: 50% 50%;
+        transform-origin: 50% 50%;
+    }
+    &:hover::after {
+        text-transform: none;
+        -webkit-transform: translateX(-50%) scaleX(1);
+        transform: translateX(-50%) scaleX(1);
+    }
 }
 
-a.link::after {
-  content: '';
-  position: absolute;
-  bottom: -2px;
-  left: 50%;
-  width: 100%;
-  height: 1px;
-  background-color: #cd6e57;
-  -webkit-transition: -webkit-transform 250ms;
-  transition: -webkit-transform 250ms;
-  transition: transform 250ms;
-  transition: transform 250ms, -webkit-transform 250ms;
-  -webkit-transform: translateX(-50%) scaleX(0);
-  transform: translateX(-50%) scaleX(0);
-  -webkit-transform-origin: 50% 50%;
-  transform-origin: 50% 50%;
-}
-
-a.link:hover::after {
-  text-transform: none;
-  -webkit-transform: translateX(-50%) scaleX(1);
-  transform: translateX(-50%) scaleX(1);
-}
 </style>
